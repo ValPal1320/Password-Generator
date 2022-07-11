84ebd852c8815132bb0b1e4bd8576cb0b012be7f
Val Pal <valgonzalez13@yahoo.com>
2022-07-10 20:23:41 -0500

    second commit

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// Assignment code here
-var enter;
-var confirmCharacter;
-var confirmNumber;
-var confirmLowercase;
-var confirmUppercase;
-
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -19,12 +10,12 @@ var passwordLength = parseInt(userInput);
 
 if (isNaN(passwordLength)) {
   alert("Input was not a number. Try again!"); 
-  return
+  return null
  } 
 
  if (passwordLength < 8 || passwordLength > 128) {
   alert("Input not valid. Try again!");
-  return
+  return null
  }
 
 //    b. Confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
@@ -34,41 +25,56 @@ var userUppercase = confirm("Would you like to include uppercase letters?");
 var userSpecialC = confirm("Would you like to include special characters?");
 
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var character = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", " \ ", "^", "_", "`", "{", "}", "|", "~"];
+var character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", " \ ", "^", "_", "`", "{", "}", "|", "~"];
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercase = [];
 
 var allOptions = []; 
 
 for (var i = 0; i < lowercase.length; i++) {
-  uppercase[i] = lowercase[i].toUpperCase()
+  uppercase.push(lowercase[i].toUpperCase())
 }
+
 // 2. Validate the user's input
+if(!userSpecialC && !userLowercase && !userUppercase && !userNumbers){
+  alert("You must choose at least one character type. Try again!");
+  return null
+}
+
 if (userNumbers === true) {
-  allOptions.push(number)
+  allOptions = allOptions.concat(number)
 }
 
 if (userLowercase === true) {
-  allOptions.push(lowercase)
+  allOptions = allOptions.concat(lowercase)
 }
 
 if (userUppercase === true) {
-  allOptions.push(uppercase)
+  allOptions = allOptions.concat(uppercase)
 }
 
 if (userSpecialC === true) {
-  allOptions.push(character)
+  allOptions = allOptions.concat(character)
 }
+
+
 // 3. Generate password based on criteria
-var generatedPassword = ""
+//    a. Run random function
+var generatedPassword = []
 
 for (var i = 0; i < passwordLength; i++) {
-
+  generatedPassword.push(random(allOptions))
 }
 
 // 4. Diplay generated password
-  return "Generated password will go here!";
+  return generatedPassword.join('');
 }
+
+// 5. create Random function to return 1 random number
+function random(array){
+  return array[Math.floor(Math.random() * array.length)]
+}
+
 
 // Write password to the #password input
 function writePassword() {
